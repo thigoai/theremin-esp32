bd32a634aa8a2147dea88e66b240b052b9a8a508
thigoai <tfreire352@gmail.com>
2026-05-02 08:44:30 -0300

    add Wi-Fi server and data transmission function

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,17 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <VL53L0X.h>
+#include <WiFi.h>
+#include <WebServer.h>
+
+WebServer server(80);
 
 VL53L0X sensor;
 int currentDistance = 0;
+
+void sentDistance(){
+  server.send(200, "text/plain", String(currentDistance));
+}
 
 void setup(){
   Serial.begin(115200);
@@ -16,10 +24,21 @@ void setup(){
   }
 
   sensor.startContinuous();
+
+  Serial.println("Initializing Wi-Fi...");
+  WiFi.softAP("Theremin-ESP", "");
+
+  Serial.print("Connect to the 'Theremin-ESP' Wi-Fi network and access the IP address: ");
+  Serial.println(WiFi.softAPIP());
+
+  server.on("/distance", sentDistance);
+
+  server.begin();
 }
 
 void loop(){
   currentDistance = sensor.readRangeContinuousMillimeters();
   Serial.print(currentDistance);
   Serial.println(" mm");
+  server.handleClient();
 }
